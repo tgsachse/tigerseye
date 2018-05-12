63bb22adef502cbcf9753722ef43533cfc69ee50
Tiger Sachse <tiger@sachse.io>
2018-05-12 14:44:19 -0400

    installs available templates

---
+++ installTemplates.sh
@@ -0,0 +1,7 @@
+installTemplate() {
+    mkdir -p $2
+    cp templates/base16-tigerseye.$1 $2
+}
+
+installTemplate sh ~/.config/base16-shell/scripts/
+installTemplate vim ~/.config/nvim/bundle/base16-vim/colors/
