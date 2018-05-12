2f557528e292b1471922dbfcd25fd3a5062f165b
Tiger Sachse <tiger@sachse.io>
2018-05-12 15:26:23 -0400

    script unneeded

--- scripts/setupEnvironment.sh
+++
@@ -1,7 +0,0 @@
-
-# Install the template builder program
-sudo apt install -y npm
-sudo npm install --global base16-builder
-
-# Install 
-git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
